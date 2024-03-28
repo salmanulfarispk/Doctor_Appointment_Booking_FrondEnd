6c2f6c3c256acc180de63c891eff37b439048582
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-03-28 16:53:53 +0530

    minor changes

--- src/userComponents/BookAppointment.jsx
+++ src/userComponents/BookAppointment.jsx
@@ -1,23 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Dialog,
+    DialogClose,
     DialogContent,
     DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
   import { Calendar } from "@/components/ui/calendar"
-import { CalendarDays } from 'lucide-react'
+import { CalendarDays, Clock } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
   
 
 export default function BookAppointment() {
 
     const [date, setDate] =useState(new Date)
+    const [TimeSlot,setTimeSlot]=useState()
+    const[selectedSlot,SetSelecetedSlot]=useState()
 
+    useEffect(()=>{
+            getTime();
+    },[])
 
-
+    const getTime = () => {
+        const timeList = [];
+        for (let i = 10; i <= 12; i++) {
+            timeList.push({
+                time: i + ':00 AM'
+            })
+            timeList.push({
+                time: i + ':30 AM'
+            })
+        }
+        for (let i = 1; i <= 6; i++) {
+            timeList.push({
+                time: i + ':00 PM'
+            })
+            timeList.push({
+                time: i + ':30 PM'
+            })
+        }
+  
+        setTimeSlot(timeList)
+      }
+  
+      const IsPastDay=(day)=>{
+        return day<new Date();
+      }
 
 
   return (
@@ -33,32 +65,51 @@ export default function BookAppointment() {
       <DialogTitle>Book Appointment</DialogTitle>
       <DialogDescription>
          <div>     
-            <div className='grid grid-cols-1 md:grid-cols-2'>
-            <div className='flex flex-col items-baseline'>
+            <div className='grid grid-cols-1 md:grid-cols-2 mt-5'>
+            <div className='flex flex-col gap-3 items-baseline'>
                 <h2 className='flex gap-2 items-center'>
-                    <CalendarDays/>
+                    <CalendarDays className='text-primary h-5 w-5'/>
                     Select Date
                 </h2>
             <Calendar
               mode="single"
               selected={date}
               onSelect={setDate}
+              disabled={IsPastDay}
               className="rounded-md border"
             />
-
-
-
-
              </div>
-              <div>
-
+            <div className='mt-1 md:mt-0'>
+                <h2 className='flex gap-2 items-center mb-3'>
+                <Clock className='text-primary h-5 w-5'/>
+                Select Time Slot
+                </h2>
+              <div className='grid grid-cols-3 gap-3 border rounded-lg px-2 py-3'>
+             {TimeSlot && TimeSlot.map((item,index)=>(
+                 <h2 key={index} className={`p-2 border  rounded-full cursor-pointer text-center
+                 hover:bg-primary hover:text-white ${item.time===selectedSlot&& 'bg-primary text-white'}`} 
+                 onClick={(()=>SetSelecetedSlot(item.time))}>{item.time}</h2>
+             ))}
  
+              </div>
               </div>
             </div>
          </div> 
 
       </DialogDescription>
     </DialogHeader>
+    <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+            <Button type="button" variant="primary">
+              Submit
+            </Button>
+            </>
+          </DialogClose>
+        </DialogFooter>
   </DialogContent>
 </Dialog>
 
