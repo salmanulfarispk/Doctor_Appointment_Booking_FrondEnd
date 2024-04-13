fc0f85e5f8aab5ca9cb4f0a3f56da771aa5c8f69
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-04-13 23:04:38 +0530

    complete userappointment listing

--- src/userComponents/BookingLIst.jsx
+++ src/userComponents/BookingLIst.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Calendar } from 'lucide-react';
+import { Clock } from 'lucide-react';
+import { MapPin } from 'lucide-react';
 
-export default function BookingLIst() {
+import moment from 'moment';
+import { Button } from '@/components/ui/button';
+
+export default function BookingLIst({bookingList,expired}) {
+
   return (
-    <div>BookingLIst</div>
+    <div>
+   {bookingList&&bookingList.map((item,index)=>(
+     <div key={index} className='flex gap-4 items-center border p-4 m-3 rounded-lg'>
+       <img src={item.image}
+       width={70}
+       height={70}
+       alt='doc-image'
+       className='rounded-full h-[70px] w-[70px] object-cover'
+       />
+     <div className='flex flex-col gap-2 w-full'>
+      <h2 className='font-bold text-[18px] items-center flex justify-between'>{item.doctorName}
+         
+         {!expired&&<Button variant='outline' className='text-primary border-primary'>Cancel Appointment</Button>}
+      
+      </h2>
+      <h2 className='flex gap-2 font-mono text-gray-500'><MapPin className='text-primary h-6 w-5' />{item.hospital}</h2>
+      <h2 className='flex gap-2 font-mono'><Calendar className='text-primary h-5 w-5'/>Appointment On : {moment(item.date).format('DD-MMM-YYYY')}</h2>
+      <h2 className='flex gap-2 font-mono'><Clock className='text-primary h-5 w-5'/> At Time : {item.time}</h2>
+
+
+      </div>
+  </div>
+   ))}
+
+
+    </div>
   )
 }
--- src/userComponents/MyBooking.jsx
+++ src/userComponents/MyBooking.jsx
@@ -1,26 +1,72 @@
-import React from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import BookingLIst from './BookingLIst'
-
+import React, { useEffect, useState } from 'react';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import BookingLIst from './BookingLIst';
+import Header from './Header';
+import axios from 'axios';
 
 export default function MyBooking() {
+  const [bookingList,setBookingList] =useState([]);
+
+   
+  const user=localStorage.getItem("userId")
+
+  const getUserBookingList = async () => {
+    try {
+      const verifyToken = {
+        headers: {
+          Authorization: `${localStorage.getItem("jwt")}`
+        }
+      };
+      const response = await axios.get("http://localhost:3001/user/users/bookingdetails", verifyToken);
+      if (response.status === 200) { 
+        setBookingList(response.data.datas)
+        
+      }
+    } catch (err) {
+      console.log(err);
+      
+    }
+  };
+
+  useEffect(() => {
+    user&&getUserBookingList();
+  }, [user]);
+
+  const filterUserBooking = (type) => {
+    const currentDate = new Date();
+    const result = bookingList.filter((item) =>
+      type === 'Upcoming' ? new Date(item.date) >= currentDate : new Date(item.date) <= currentDate
+    );
+    // console.log("result", result);
+    return result;
+  };
+
+
   return (
-    <div className='px-4 sm:px-10 mt-10'>
-      <h2 className='font-bold text-2xl'>My Booking</h2>
-      <Tabs defaultValue="Upcoming" className="w-full">
-      <TabsList className='w-full justify-start'>
-    <TabsTrigger value="Upcoming">Upcoming</TabsTrigger>
-    <TabsTrigger value="Expired">Expired</TabsTrigger>
-  </TabsList>
-  <TabsContent value="Upcoming">
-    <BookingLIst/>
-  </TabsContent>
-  <TabsContent value="Expired">
-    <BookingLIst/>
-  </TabsContent>
-</Tabs>
+    <>
+      <div className="md:px-10">
+        <Header />
+      </div>
 
-
-    </div>
-  )
+      <div className='px-8 sm:px-10 mt-10'>
+        <h2 className='font-bold text-2xl mb-2'>My Booking</h2>
+        <Tabs defaultValue="Upcoming" className="w-full">
+          <TabsList className='w-full justify-start'>
+            <TabsTrigger value="Upcoming">Upcoming</TabsTrigger>
+            <TabsTrigger value="Expired">Expired</TabsTrigger>
+          </TabsList>
+          <TabsContent value="Upcoming">
+            <BookingLIst bookingList={filterUserBooking('Upcoming')} 
+             expired={false}
+             />
+          </TabsContent>
+          <TabsContent value="Expired">
+            <BookingLIst bookingList={filterUserBooking('Expired')} 
+            expired={true}
+            />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
